120d2ba94e509ea8f26d8055b44f32afbdc22166
Blake Worsley <blakeworsley@gmail.com>
2016-10-24 14:14:16 -0600

    added enzyme and created a Register component and test file

---
+++ __tests__/Register-Test.js
@@ -0,0 +1,17 @@
+import React, { View } from 'react-native';
+import Register from '../components/Register';
+import { shallow } from 'enzyme';
+
+import renderer from 'react-test-renderer';
+
+it('renders correctly', () => {
+  const tree = renderer.create(
+    <Register />
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('renders a form', () => {
+  const wrapper = shallow(<Register></Register>)
+  expect(wrapper.type()).to.equal(View);
+})
---
+++ __tests__/SwimmerDashboard-Test.js
@@ -0,0 +1,17 @@
+import 'react-native';
+import React from 'react';
+import SwimmerDashboard from '../components/SwimmerDashboard';
+
+// Note: test renderer must be required after react-native.
+import renderer from 'react-test-renderer';
+
+it('renders correctly', () => {
+  const tree = renderer.create(
+    <SwimmerDashboard />
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('renders a form on home page', () => {
+
+})
