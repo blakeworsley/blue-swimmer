d6194e6157bfbaccafeb5a5ee54e3a9dd3a50e44
Blake Worsley <blakeworsley@gmail.com>
2016-10-29 12:53:54 -0600

    broke react native

--- app/components/SwimmerDashboard.js
+++ app/components/SwimmerDashboard.js
@@ -1,8 +1,7 @@
 'use strict';
 
 import React, { Component } from 'react';
-import StarRating from 'react-native-star-rating';
-import { StyleSheet, Text, View }  from 'react-native';
+import { StyleSheet, Text, View, TextInput }  from 'react-native';
 const styles = require('../styles.js');
 const constants = styles.constants;
 
@@ -10,26 +9,59 @@ class SwimmerDashboard extends Component {
   constructor(props) {
      super(props);
      this.state = {
-       starCount: 3.5
+       physical: null,
+       mental: null,
+       effort: null,
+       date: Date.now()
      };
    }
 
-   onStarRatingPress(rating) {
-      this.setState({
-        starCount: rating
-      });
-    }
-
   render() {
     return (
-      <View>
+      <View style={styles.container}>
         <Text style={styles.swimmerDashboard}>{this.props.title}</Text>
-        <StarRating
-               disabled={false}
-               maxStars={5}
-               rating={this.state.starCount}
-               selectedStar={(rating) => this.onStarRatingPress(rating)}
-             />
+          <Text>Physical</Text>
+          <TextInput
+            ref="1"
+            style={styles.newUserInput}
+            onChangeText={(raiting) => this.setState({raiting})}
+            value={this.state.physical}
+            placeholder="Rating between 1 and 5"
+            keyboardType="numeric"
+            returnKeyType="next"
+            onSubmitEditing={() => this.focusNextField('2')}
+            autoCapitalize="none"
+          />
+          <Text>Mental</Text>
+          <TextInput
+            ref="2"
+            style={styles.newUserInput}
+            onChangeText={(raiting) => this.setState({raiting})}
+            value={this.state.mental}
+            placeholder="Rating between 1 and 5"
+            keyboardType="numeric"
+            returnKeyType="next"
+            onSubmitEditing={() => this.focusNextField('3')}
+            autoCapitalize="none"
+          />
+          <Text>Effort</Text>
+          <TextInput
+            ref="2"
+            style={styles.newUserInput}
+            onChangeText={(raiting) => this.setState({raiting})}
+            value={this.state.effort}
+            placeholder="Rating between 1 and 5"
+            keyboardType="numeric"
+            returnKeyType="done"
+            onSubmitEditing={() => this.focusNextField('submit')}
+            autoCapitalize="none"
+          />
+          <TouchableHighlight
+            ref="submit"
+            style={styles.button}
+          >
+          <Text>Submit</Text>
+          </TouchableHighlight>
       </View>
     );
   }
